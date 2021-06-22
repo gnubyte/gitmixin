d36b1d53ee87e5abdb31a8dd87e16eb05c7711ec
Patrick Hastings <phastings@openmobo.com>
2021-06-22 00:25:29 -0400

    setup file ready

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gitmixin",
+    version="0.1.0",
+    author="Patrick Hastings",
+    author_email="phastings@gnubyte.com",
+    description="An SQL-Alchemy mixin to give records per row a git version tracked text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gnubyte/gitmixin",
+    project_urls={
+        "Bug Tracker": "https://github.com/gnubyte/gitmixin/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="gitmixin"),
+    python_requires=">=3.6",
+)
